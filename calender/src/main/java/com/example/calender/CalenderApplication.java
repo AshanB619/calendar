ac861ed23b1a70bb4d2ed88d3ab2027c60335d2e
AshanB619 <162773718+AshanB619@users.noreply.github.com>
2024-10-21 19:32:13 +0530

    update

--- calender/src/main/java/com/example/calender/CalenderApplication.java
+++ calender/src/main/java/com/example/calender/CalenderApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CalenderApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context=SpringApplication.run(CalenderApplication.class, args);
+		RestTemplate restTemplate=(RestTemplate) context.getBean("restTemplate");
+		System.out.println(restTemplate);
 
 	}
 
