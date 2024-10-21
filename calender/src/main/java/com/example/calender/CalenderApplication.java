06acae2bd1645e6c08265563f3eb298864069029
AshanB619 <162773718+AshanB619@users.noreply.github.com>
2024-10-21 17:13:24 +0530

    upadte

--- calender/src/main/java/com/example/calender/CalenderApplication.java
+++ calender/src/main/java/com/example/calender/CalenderApplication.java
@@ -1,13 +1,16 @@
 package com.example.calender;
 
+import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CalenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalenderApplication.class, args);
+		ConfigurableApplicationContext context=SpringApplication.run(CalenderApplication.class, args);
+
 	}
 
 }
---
+++ calender/src/main/java/com/example/calender/message.java
@@ -0,0 +1,10 @@
+package com.example.calender;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class message {
+    public String getmessage() {
+        return "hello";
+    }
+}
